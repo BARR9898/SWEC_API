22a168af90bf3e5a99ab8da5c272de8908641b53
Brayan Rodriguez <brayanrodriguez@Brayans-MacBook-Air.local>
2023-05-17 09:57:58 -0600

    Eliminacion de consoles.log

--- src/utils/jwt.handle.ts
+++ src/utils/jwt.handle.ts
@@ -3,7 +3,7 @@ import { sign,verify } from "jsonwebtoken";
 const JWT_SECRET = process.env.JWT_SECRET || "token";
 
 const generateToken =  (id:string) => {
-    const jwt =  sign({id},JWT_SECRET,{expiresIn: "2h"});
+    const jwt =  sign({id},JWT_SECRET,{expiresIn: "8h"});
     return jwt;
 }
 
